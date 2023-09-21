c41437e66d8222c5b87cd704d5c67189afe96902
Xngv <1364761092@qq.com>
2023-09-21 16:14:44 +0800

    update:订阅时的控制台输出相关信息

--- subscribe.py
+++ subscribe.py
@@ -1,12 +1,14 @@
 """
 @author Bcai
 """
-import time
+import datetime
+
 import butils
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 def subscribe_email():
+    print(f'订阅最新文章提醒脚本-start：{datetime.datetime.now()}')
     subscribers = butils.get_subscribers_list()
     template = butils.get_email_template('newest-paper-reminder-1')
     for s in subscribers:
@@ -18,10 +20,12 @@ def subscribe_email():
                 # 发送邮件
                 content = template.format(papers[0]['title'], papers[0]['authors'], papers[0]['pdf_url'])
                 butils.send_email(s, cfg['e-mail'], '新文章提醒', content)
+                print(f'向{s}: {cfg["e-mail"]}发送了新邮件：\n{content}\n')
+    print(f'订阅最新文章提醒脚本-end：{datetime.datetime.now()}')
 
 
 if __name__ == '__main__':
     # 调度框架每周一和周四早上7:00执行一次
     scheduler = BlockingScheduler()
-    scheduler.add_job(subscribe_email, 'cron', max_instances=1, day_of_week='mon,thu', hour=7, minute=00)
+    scheduler.add_job(subscribe_email, 'cron', max_instances=1, day_of_week='mon,thu', hour=7, minute=0)
     scheduler.start()
